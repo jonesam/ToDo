409401f25c244b2ac05249c7cc9948200269fe25
jonesam <30280354+jonesam@users.noreply.github.com>
2017-07-19 17:34:34 +1200

    start of app
    created simple job page with add  and display feature

---
+++ app/src/main/java/com/example/sherwin/todo/Job.java
@@ -0,0 +1,48 @@
+package com.example.sherwin.todo;
+
+/**
+ * Created by Sherwin on 18/07/2017.
+ */
+
+public class Job {
+
+    String jobNum;
+    String jobDate;
+    String jobPri;
+    String jobDeet;
+    String jobMch1;
+    String jobMch2;
+
+    public Job(String jobNum, String jobDate, String jobPri, String jobDeet, String jobMch1, String jobMch2) {
+        this.jobNum = jobNum;
+        this.jobDate = jobDate;
+        this.jobPri = jobPri;
+        this.jobDeet = jobDeet;
+        this.jobMch1 = jobMch1;
+        this.jobMch2 = jobMch2;
+    }
+
+    public String getJobNum() {
+        return jobNum;
+    }
+
+    public String getJobDate() {
+        return jobDate;
+    }
+
+    public String getJobPri() {
+        return jobPri;
+    }
+
+    public String getJobDeet() {
+        return jobDeet;
+    }
+
+    public String getJobMch1() {
+        return jobMch1;
+    }
+
+    public String getJobMch2() {
+        return jobMch2;
+    }
+}
---
+++ app/src/main/java/com/example/sherwin/todo/JobForm.java
@@ -0,0 +1,46 @@
+package com.example.sherwin.todo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class JobForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_form);
+
+        // Connect to the Firebase database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // Add items via the Button and EditText at the bottom of the window.
+        final EditText jobNum = (EditText) findViewById(R.id.job_id);
+        final EditText jobDate = (EditText) findViewById(R.id.date_now);
+        final EditText jobPri = (EditText) findViewById(R.id.job_imp);
+        final EditText jobDeet = (EditText) findViewById(R.id.job_dis);
+        final EditText jobMch1 = (EditText) findViewById(R.id.first_machine);
+        final EditText jobMch2 = (EditText) findViewById(R.id.second_machine);
+
+        final DatabaseReference job1 = database.getReference("JOBS");
+        final Button but_sub = (Button) findViewById(R.id.but_sub);
+        but_sub.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // you must creat a job class
+                // asumming you did it will have the toString()method in ever parmeteter in the constracter
+                Job job = new Job(jobNum.getText().toString(),jobDate.getText().toString(),jobPri.getText().toString(),jobDeet.getText().toString(),jobMch1.getText().toString(),jobMch2.getText().toString());
+                job1.child(jobNum.getText().toString()).setValue(job);
+        // Get a reference to the child items it the database
+
+
+
+            }
+        });
+
+
+    }
+}
