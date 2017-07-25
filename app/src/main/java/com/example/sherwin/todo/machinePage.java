33b8992bfe686fb3d031521a37f251b6f60657ea
saviocreado <30282521+saviocreado@users.noreply.github.com>
2017-07-25 18:10:16 +1200

    changed but to men

--- app/src/main/java/com/example/sherwin/todo/machinePage.java
+++ app/src/main/java/com/example/sherwin/todo/machinePage.java
@@ -17,7 +17,7 @@ public class machinePage extends AppCompatActivity {
         final ImageButton jobMenuBut = (ImageButton) findViewById(R.id.men_job);
         jobMenuBut.setOnClickListener( new View.OnClickListener(){
             public void onClick(View v){
-                Intent goToJobList = new Intent (getBaseContext(),MainActivity.class);
+                Intent goToJobList = new Intent (machinePage.this,MainActivity.class);
                 startActivity(goToJobList);
             }
         });
@@ -25,7 +25,7 @@ public class machinePage extends AppCompatActivity {
         final ImageButton mailMenuBut = (ImageButton) findViewById(R.id.men_msg);
         mailMenuBut.setOnClickListener( new View.OnClickListener(){
             public void onClick(View v){
-                Intent goToMsgList = new Intent (getBaseContext(),messagePage.class);
+                Intent goToMsgList = new Intent (machinePage.this,messagePage.class);
                 startActivity(goToMsgList);
             }
         });
@@ -33,7 +33,7 @@ public class machinePage extends AppCompatActivity {
         final ImageButton resMenuBut = (ImageButton) findViewById(R.id.men_res);
         resMenuBut.setOnClickListener( new View.OnClickListener(){
             public void onClick(View v){
-                Intent goToResList = new Intent (getBaseContext(),resourcePage.class);
+                Intent goToResList = new Intent (machinePage.this,resourcePage.class);
                 startActivity(goToResList);
             }
         });
@@ -41,7 +41,7 @@ public class machinePage extends AppCompatActivity {
         final ImageButton mchMenuBut = (ImageButton) findViewById(R.id.men_mch);
         mchMenuBut.setOnClickListener( new View.OnClickListener(){
             public void onClick(View v){
-                Intent goToMchList = new Intent (getBaseContext(),machinePage.class);
+                Intent goToMchList = new Intent (machinePage.this,machinePage.class);
                 startActivity(goToMchList);
             }
         });
@@ -50,7 +50,7 @@ public class machinePage extends AppCompatActivity {
         homeMenuBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToHome = new Intent (getBaseContext(), homePage.class);
+                Intent goToHome = new Intent (machinePage.this, homePage.class);
                 startActivity(goToHome);
             }
         });
