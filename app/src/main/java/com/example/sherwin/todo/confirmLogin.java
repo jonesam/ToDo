048880737010f5e715ab302dd554c183d060d4fc
jonesam <30280354+jonesam@users.noreply.github.com>
2017-08-07 13:59:49 +1200

    ui
    made it neater

--- app/src/main/java/com/example/sherwin/todo/confirmLogin.java
+++ app/src/main/java/com/example/sherwin/todo/confirmLogin.java
@@ -1,22 +1,22 @@
 package com.example.sherwin.todo;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class loginPage extends AppCompatActivity {
+public class confirmLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_confirm_login);
 
-        Button toConfLog = (Button) findViewById(R.id.but_login);
-        toConfLog.setOnClickListener(new View.OnClickListener(){
+        Button toMain = (Button) findViewById(R.id.confirm_login);
+        toMain.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Intent myIntent = new Intent(loginPage.this,confirmLogin.class);
+                Intent myIntent = new Intent(confirmLogin.this,MainActivity.class);
                 startActivity(myIntent);
             }
 
